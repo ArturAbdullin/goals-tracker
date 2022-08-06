67277e649ecd314a3b074db1b1a73473e5227bae
Artur Abdullin <artur.abdullin@gmail.com>
2022-08-06 14:33:02 +0300

    feat: add CourseInput component

---
+++ src/Components/CourseGoals/CourseInput/CourseInput.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from "react";
+import { useState } from "react";
+
+import "./CourseInput.css";
+
+type CourseInputProps = {
+  onAddGoal?: (goal: string) => void;
+};
+
+const CourseInput: FC<CourseInputProps> = (props) => {
+  const [enteredValue, setEnteredValue] = useState<string>("");
+
+  const goalInputChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    props.onAddGoal?.(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label htmlFor="">Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+    </form>
+  );
+};
+
+export default CourseInput;

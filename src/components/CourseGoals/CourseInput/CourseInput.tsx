2bbfeab10bb5975246c1f2c232d2b5594ccff08d
Artur Abdullin <artur.abdullin@gmail.com>
2022-08-08 23:26:37 +0300

    feat(CourseInput): use styled FormControl component

--- src/components/CourseGoals/CourseInput/CourseInput.tsx
+++ src/components/CourseGoals/CourseInput/CourseInput.tsx
@@ -1,9 +1,42 @@
 import React, { FC } from "react";
 import { useState } from "react";
+import styled from "styled-components";
 import { AddGoalEventHandler } from "../../../models/eventHandlers";
 import Button from "../../UI/Button";
-
 import "./CourseInput.css";
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &.invalid input {
+    border-color: red;
+  }
+
+  &.invalid label {
+    color: red;
+  }
+`;
 
 type CourseInputProps = {
   onAddGoal: AddGoalEventHandler;
@@ -31,10 +64,10 @@ const CourseInput: FC<CourseInputProps> = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid && "invalid"}`}>
+      <FormControl className={isValid ? "" : "invalid"}>
         <label htmlFor="">Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );

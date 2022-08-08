6f27c0e03b21ab922723444f9ec5a30915fed8bf
Artur Abdullin <artur.abdullin@gmail.com>
2022-08-08 19:07:32 +0300

    feat(App): add basic structure and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import CourseInput from "./components/CourseGoals/CourseInput/CourseInput";
+
+import './App.css';
 
 function App() {
+  let content = (
+    <p style={{ textAlign: "center" }}>No goals found. Maybe add one?</p>
+  );
+
   return (
-    <></>
-  )
+    <div>
+      <section id="goal-form">
+        <CourseInput onAddGoal={() => {}} />
+      </section>
+      <section id="goals">{content}</section>
+    </div>
+  );
 }
 
 export default App;

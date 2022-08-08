12e7d9d5a303be43187cffeaa645aaf456a97796
Artur Abdullin <artur.abdullin@gmail.com>
2022-08-08 20:58:28 +0300

    feat(CourseInput): add input validation
    changing input style dynamically

--- src/components/CourseGoals/CourseInput/CourseInput.tsx
+++ src/components/CourseGoals/CourseInput/CourseInput.tsx
@@ -11,21 +11,27 @@ type CourseInputProps = {
 
 const CourseInput: FC<CourseInputProps> = (props) => {
   const [enteredValue, setEnteredValue] = useState<string>("");
+  const [isValid, setIsValid] = useState<boolean>(true);
 
   const goalInputChangeHandler = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
+    if (event.target.value.trim().length > 0) setIsValid(true);
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid && "invalid"}`}>
         <label htmlFor="">Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
